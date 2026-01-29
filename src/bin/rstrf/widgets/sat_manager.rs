cadd84c9fe5fd9ebe4dcbf52e14c4371c8c3e5d2
jazzpi <jasper@mezzo.de>
2026-01-29 17:54:39 +0100

    feat: allow toggling satellite visibility

--- src/bin/rstrf/widgets/sat_manager.rs
+++ src/bin/rstrf/widgets/sat_manager.rs
@@ -3,7 +3,7 @@ use std::path::PathBuf;
 use iced::{
     Element, Length, Task,
     alignment::{Horizontal, Vertical},
-    widget::{container, text},
+    widget::{checkbox, container, scrollable, table, text},
 };
 use rstrf::orbit::Satellite;
 
@@ -16,10 +16,11 @@ pub enum Message {
     SatellitesLoaded(Result<Vec<Satellite>, String>),
     /// For notifying the parent that the satellite list has changed
     SatellitesChanged(Vec<Satellite>),
+    SatelliteToggled(usize, bool),
 }
 
 pub struct SatManager {
-    satellites: Vec<Satellite>,
+    satellites: Vec<(Satellite, bool)>,
 }
 
 impl SatManager {
@@ -30,11 +31,39 @@ impl SatManager {
     }
 
     pub fn view(&self) -> Element<'_, Message> {
-        container(text("Satellite Manager - TODO"))
+        let columns = [
+            table::column(
+                text("Norad ID"),
+                |(_, (sat, _)): (usize, &(Satellite, bool))| text(sat.norad_id().to_string()),
+            ),
+            table::column(
+                text("Name"),
+                |(_, (sat, _)): (usize, &(Satellite, bool))| {
+                    text(
+                        sat.elements
+                            .object_name
+                            .clone()
+                            .unwrap_or("N/A".to_string()),
+                    )
+                },
+            ),
+            table::column(
+                text("Frequency (MHz)"),
+                |(_, (sat, _)): (usize, &(Satellite, bool))| {
+                    text(format!("{:3}", sat.tx_freq / 1e6))
+                },
+            ),
+            table::column(
+                text("Show"),
+                |(idx, (_, active)): (usize, &(Satellite, bool))| {
+                    checkbox(*active)
+                        .on_toggle(move |new_state| Message::SatelliteToggled(idx, new_state))
+                },
+            ),
+        ];
+        scrollable(table(columns, self.satellites.iter().enumerate()))
             .width(Length::Fill)
             .height(Length::Fill)
-            .align_x(Horizontal::Center)
-            .align_y(Vertical::Center)
             .into()
     }
 
@@ -54,7 +83,7 @@ impl SatManager {
             Message::SatellitesLoaded(satellites) => match satellites {
                 Ok(satellites) => {
                     log::info!("Loaded {} satellites", satellites.len());
-                    self.satellites = satellites;
+                    self.satellites = satellites.into_iter().map(|sat| (sat, true)).collect();
                     Task::done(Message::SatellitesChanged(self.satellites().to_vec()))
                 }
                 Err(err) => {
@@ -62,11 +91,22 @@ impl SatManager {
                     Task::none()
                 }
             },
-            Message::SatellitesChanged(_) => Task::none(), // This message is intended for the parent
+            Message::SatellitesChanged(_) => Task::none(),
+            Message::SatelliteToggled(idx, active) => {
+                let Some((_, sat_active)) = self.satellites.get_mut(idx) else {
+                    log::error!("Invalid satellite index toggled: {}", idx);
+                    return Task::none();
+                };
+                *sat_active = active;
+                Task::done(Message::SatellitesChanged(self.satellites()))
+            }
         }
     }
 
-    pub fn satellites(&self) -> &[Satellite] {
-        &self.satellites
+    pub fn satellites(&self) -> Vec<Satellite> {
+        self.satellites
+            .iter()
+            .filter_map(|(sat, active)| if *active { Some(sat.clone()) } else { None })
+            .collect::<Vec<_>>()
     }
 }
