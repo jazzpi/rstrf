64a82274da8bc539fd2d40bd2363a9b90d19d40a
jazzpi <jasper@mezzo.de>
2026-07-06 16:39:11 +0200

    chore: formatting

--- flake.nix
+++ flake.nix
@@ -97,7 +97,12 @@
             rustup
             rustPlatform.bindgenHook
             pkg-config
-            (python3.withPackages (ps: with ps; [ numpy skyfield ]))
+            (python3.withPackages (
+              ps: with ps; [
+                numpy
+                skyfield
+              ]
+            ))
           ];
           inherit buildInputs;
           RUSTC_VERSION = "stable";
