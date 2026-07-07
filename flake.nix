62113a289d21578800d2e9e05adbdbc249417ed6
jazzpi <jasper@mezzo.de>
2026-07-07 20:24:39 +0200

    build: use nightly rustfmt
    stable rustfmt silently fails if it can't format code to <max_width
    (usually because of a long string literal nearby), and leaves the AST
    leaf(?) unmodified. nightly rustfmt has an option to error on line
    overflow.

--- flake.nix
+++ flake.nix
@@ -105,15 +105,11 @@
             ))
           ];
           inherit buildInputs;
-          RUSTC_VERSION = "stable";
           shellHook =
             let
-              toolchainPath = "\${RUSTUP_HOME:-$HOME/.rustup}/toolchains/${RUSTC_VERSION}-${stdenv.hostPlatform.rust.rustcTarget}";
               ldLibraryPath = lib.makeLibraryPath runtimeDependencies;
             in
             ''
-              export PATH="${toolchainPath}/bin:$PATH"
-              export RUSTC="${toolchainPath}/bin/rustc"
               export LD_LIBRARY_PATH="${ldLibraryPath}:$LD_LIBRARY_PATH"
               export __EGL_VENDOR_LIBRARY_FILENAMES=${mesa}/share/glvnd/egl_vendor.d/50_mesa.json
               export RUST_BACKTRACE=1
