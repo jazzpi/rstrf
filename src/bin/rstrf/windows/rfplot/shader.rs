49333a911b46b3bab00b28bb12151b39ecc143f0
jazzpi <jasper@mezzo.de>
2026-07-08 22:18:15 +0200

    fix: use <= for depth compare
    In the fragment shader, we clip the depth value to 1.0 for values below
    min power. With the < comparison, that meant the depth test would fail
    for those fragments. Thus, the background color would be rendered
    instead of the minimum value from the colormap.

--- src/bin/rstrf/windows/rfplot/shader.rs
+++ src/bin/rstrf/windows/rfplot/shader.rs
@@ -97,7 +97,7 @@ impl shader::Pipeline for Pipeline {
             depth_stencil: Some(wgpu::DepthStencilState {
                 format: DEPTH_FORMAT,
                 depth_write_enabled: true,
-                depth_compare: wgpu::CompareFunction::Less,
+                depth_compare: wgpu::CompareFunction::LessEqual,
                 stencil: wgpu::StencilState::default(),
                 bias: wgpu::DepthBiasState::default(),
             }),
